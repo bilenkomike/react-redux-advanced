a081d7953a48b6dc8b392e4d37404bdb00168ede
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-04 11:44:26 +0200

    completting the redux cart funcs

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,46 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My First Book',
+    description: " This is my first book I`ve ever wrote!",
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: 'My Second Book',
+    description: " This is my second book I`ve ever wrote!",
+  },
+  {
+    id: 'p3',
+    price: 19,
+    title: 'My Third Book',
+    description: " This is my third book I`ve ever wrote!",
+  },
+  {
+    id: 'p4',
+    price: 7,
+    title: 'My Fourth Book',
+    description: " This is my fourth book I`ve ever wrote!",
+  },
+];
+
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUTS.map(product => <ProductItem
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+        />)}
       </ul>
     </section>
   );
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // slices
 import uiSlice from './slices/ui-slice';
+import cartSlice from './slices/cart-slice';
 
 const store = configureStore({
     reducer: {
         ui: uiSlice.reducer,
+        cart: cartSlice.reducer
     }
 });
 
